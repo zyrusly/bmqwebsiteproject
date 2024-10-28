204261608a0b0b532d9cd2e49adfbfeae6454e91
Zyro Dev <zyrezyro@gmail.com>
2024-10-28 17:42:50 +0800

    3rd commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -44,9 +44,9 @@ $app->singleton(
     App\Exceptions\Handler::class
 );
 
-$app->withMiddleware(function (Middleware $middleware) {
-    $middleware->trustProxies(at: '*');
-});
+// $app->withMiddleware(function (Middleware $middleware) {
+//     $middleware->trustProxies(at: '*');
+// });
 
 /*
 |--------------------------------------------------------------------------
