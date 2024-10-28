438d780a5af262246a2edc98c887940c59aa50dd
Zyro Dev <zyrezyro@gmail.com>
2024-10-28 18:58:14 +0800

    4th commit

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -21,8 +21,8 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     {{-- local styles --}}
-    <link href="{{asset('css/index.css')}}" rel="css/stylesheet">
-    <link href="{{asset('css/preloader.css')}}" rel="css/stylesheet">
+    <link href="{{asset('css/index.css')}}" rel="stylesheet">
+    <link href="{{asset('css/preloader.css')}}" rel="stylesheet">
 
     @yield('recaptcha')
     
